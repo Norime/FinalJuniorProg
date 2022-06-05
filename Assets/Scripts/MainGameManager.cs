b977e912bcf0a4170c548e3d3722cf023ccd03ae
loic <loic.gondet@hotmail.fr>
2022-06-06 00:37:56 +0200

    Name in Game Scene

--- Assets/Scripts/MainGameManager.cs
+++ Assets/Scripts/MainGameManager.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MainGameManager : MonoBehaviour
 {
+    public TextMeshProUGUI playerNameText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerNameText.text = GameManager.Instance.playerName;
     }
 
     // Update is called once per frame
